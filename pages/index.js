47d1b7ee078f207075967fef1d2684fe0105eab9
Stan <stanislav.vranic@km2.de>
2022-11-18 11:26:07 +0100

    Content Delivery API Setup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
 import styles from '../styles/Home.module.css';
 import SelectSection from '../components/selectsection';
+import { createClient } from 'contentful'
 
-export default function Home() {
+export async function getStaticProps() {
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    environment: process.env.CONTENTFUL_ENVIRONMENT_ID,
+  })
+
+  const jobList = await client.getEntries({ content_type: "job" })
+
+  return {
+    props: {
+      jobs: jobList.items,
+    }
+  }
+}
+export default function Home({jobs}) {
+  console.log(jobs);
   return (
     <div className={styles.headerCareer}>
       <p className={styles.description}>
